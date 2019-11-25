c1f12cb0ddd8c8c032c07104c62d36d55e1e3beb
menglijinghua <508304482@qq.com>
2019-11-25 21:51:33 +0800

    all

--- lab5/sortTree.cpp
+++ lab5/sortTree.cpp
@@ -1,24 +1,48 @@
 #include<iostream>
 #include<cstdlib>
 #include<vector>
-#include<algorithm> 
+#include<algorithm>
 using namespace std;
-typedef struct Node{
+typedef struct Node {
 	int data;
 	struct Node *lchild;
 	struct Node *rchild;
-}Node,*PNode;
-int main(){
-	
+} Node,*PNode;
+bool initTree(PNode &p,vector<int> v,int &i);
+void disp(PNode &p);
+int main() {
+	PNode p=NULL;
+	int i=0;
+	vector<int> v{5,4,2,1,7,3,6};
+	initTree(p,v,i);
+	disp(p);
+	return 0;
 }
-bool createTree(PNode &P){
-	P=(PNode)malloc(sizeof(Node));
-	P->lchild=NULL;
-	P->rchild=NULL;
-	P->data=0;	
+bool initTree(PNode &p,vector<int> v,int &i) {
+	if(i>=v.size()){
+		return false;
+	}
+	if(p==NULL) {
+		PNode p=(PNode)malloc(sizeof(Node));
+		p->lchild=NULL;
+		p->rchild=NULL;
+		p->data=v[i];
+		i++;
+		if(v[i]>p->data) {
+			initTree(p->rchild,v,i);
+		} else {
+			initTree(p->lchild,v,i);
+		}
+	}
 	return true;
 }
 
-bool initTree(PNode &P,vector<int> v,int &pos){
-	
+void disp(PNode &p) {
+	if(p->lchild) {
+		disp(p->lchild);
+	}
+	cout<<p->data<<" ";
+	if(p->rchild) {
+		disp(p->rchild);
+	}
 }
