ceed8e0585da700d7a89bacfd1118d71d65a864c
menglijinghua <508304482@qq.com>
2019-12-10 23:40:15 +0800

    all

---
+++ collection1/countNun.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+	int n,a;
+	cin>>n;
+	vector<int> v;
+	for(int i=0;i<n;++i){
+		cin>>a;
+		v.push_back(a);
+	}
+	sort(v.begin();v.end());
+	for()
+	
+} 
