f65107621d3a9920ce03f96c85619cf739b70b25
menglijinghua <508304482@qq.com>
2020-01-08 08:35:16 +0800

    8

--- test/test1.cpp
+++ test/test1.cpp
@@ -1,14 +1,20 @@
 #include<iostream>
 #include<string>
+#include<typeinfo>
 using namespace std;
 
 int main()
 
 {
 	
-	string str;
-	getline(cin,str);
-	cout<<str.length();
+	string str="fds";
+	const string str1="fedasfd";
+	bool f=typeid(str1).name()==typeid(str).name();
+	cout<<f<<endl;
+	
+//	string str;
+//	getline(cin,str);
+//	cout<<str.length();
 	return 0; 
 
 }
