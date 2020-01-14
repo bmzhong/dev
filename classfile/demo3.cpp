794bd449f2e1cdc635f9e2e680d6568200d74ba8
menglijinghua <508304482@qq.com>
2020-01-14 18:29:12 +0800

    14

--- classfile/demo3.cpp
+++ classfile/demo3.cpp
@@ -1,45 +1,72 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class Complex{
-    public:
-        static int real;
-        char *c;
-        int imag;
-    Complex(int _real){
-        cout<<"我是类型转换构造函数"<<endl;
-        real=_real;
-        imag=0;
+class Complex
+{
+public:
+    static int real;
+    char *c;
+    int imag;
+    int f1();
+    static int f2();
+    void f3() const;
+    Complex(int _real)
+    {
+        cout << "类型转换构造函数" << endl;
+        real = _real;
+        imag = 0;
     }
-    Complex(int _real,int _imag){
-        cout<<"我是有两个参数的构造函数"<<endl;
-        real=_real;
-        imag=_imag;
+    Complex(int _real, int _imag)
+    {
+        cout << "有两个参数的构造函数" << endl;
+        real = _real;
+        imag = _imag;
     }
-    Complex(){
-
+    Complex()
+    {
     }
     ~Complex();
 };
-Complex::~Complex(){
-    cout<<"我是析构函数"<<endl;
+
+void Complex::f3() const{
+    cout<<"hello world"<<endl;
 }
 
-Complex func(Complex c){
-    cout<<"我是一个函数"<<endl;
+int Complex::f1()
+{
+    return 1;
+}
+
+int Complex::f2()
+{
+    return 2;
+}
+
+int Complex::real = 0;
+Complex::~Complex()
+{
+    cout << "我是析构函数" << endl;
+}
+
+Complex func(Complex c)
+{
+    cout << "我是一个函数" << endl;
     Complex d;
     return d;
-
 }
 
-int main(){
-    cout<<Complex::real<<endl;
-    Complex *p1=new Complex(4,5);
-    Complex *p=new Complex[2];
-    delete [] p;
+int main()
+{
+    const Complex q;
+    q.f3();
+    cout<<"------------------------"<<endl;
+    cout << Complex::f2() << endl;
+    Complex *p1 = new Complex(4, 5);
+    Complex *p = new Complex[2];
+    delete[] p;
     delete p1;
     Complex a;
-    cout<<"------------------------"<<endl;
-    Complex d=func(a);
+    cout << "------------------------" << endl;
+    Complex d = func(a);
     system("pause");
     return 0;
 }
