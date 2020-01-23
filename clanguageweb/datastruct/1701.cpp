637c11812e3e39d5cec3a670b5d44c9be04a5b16
menglijinghua <508304482@qq.com>
2020-01-23 15:32:55 +0800

    22

--- clanguageweb/datastruct/1701.cpp
+++ clanguageweb/datastruct/1701.cpp
@@ -58,17 +58,17 @@ void createTHTree(PN &pn, int n)
 void select(PN &pn, int e, int &s1, int &s2)
 {
     int i = 1;
-    while (pn[i].parent&&i<=e)//注意是pn[i].parent而不是！pn[i].parent
+    while (pn[i].parent && i <= e) //注意是pn[i].parent而不是！pn[i].parent
     {
         ++i;
     }
     s1 = i++;
-    while (pn[i].parent&&i<=e)
+    while (pn[i].parent && i <= e)
     {
         ++i;
     }
     s2 = i++;
-    if (pn[s1].weight > pn[s2].weight)//注意这里的判断条件是pn[s1].weight > pn[s2].weight而不是s1>s2;
+    if (pn[s1].weight > pn[s2].weight) //注意这里的判断条件是pn[s1].weight > pn[s2].weight而不是s1>s2;
     {
         int t;
         t = s1;
@@ -85,12 +85,12 @@ void select(PN &pn, int e, int &s1, int &s2)
             {
                 s2 = s1;
                 s1 = i;
-                continue;//注意如果不加continue，会影响 else if (pn[i].weight < pn[s2].weight)的判断。
+                //continue;//注意如果不加continue，会影响 else if (pn[i].weight < pn[s2].weight)的判断。
             }
             else if (pn[i].weight < pn[s2].weight)
                 s2 = i;
-        // 不能写成pn[i].weight > pn[s2].weight&&pn[i].weight < pn[s2].weight.
-        //要考虑pn[i].weight == pn[s2].weight的情况。
+            // 不能写成pn[i].weight > pn[s2].weight&&pn[i].weight < pn[s2].weight.
+            //要考虑pn[i].weight == pn[s2].weight的情况。
         }
     }
     if (s1 > s2)
@@ -103,26 +103,42 @@ void select(PN &pn, int e, int &s1, int &s2)
 
 void coding(PN &pn, int n, vector<string> &v)
 {
-    for (int i = 1; i <= n; ++i)
+    int m = 2 * n - 1;
+    v.resize(n+1);
+    for (int i = 1; i <= m; ++i)
     {
-        string str;
-        int j = i;
-        int k = pn[i].parent;
-        while (k)
+        pn[i].weight = 0;
+    }
+    string str = "";
+    while (m)
+    {
+        if (pn[m].weight == 0)
         {
-            pn[k].lchild == j ? str += "0" : str += "1";
-            j = k;
-            k = pn[k].parent;
+            pn[m].weight = 1;
+            if (pn[m].lchild)
+            {
+                m = pn[m].lchild;
+                str += "0";
+            }else if(!pn[m].rchild){
+                v[m]=str;
+            }
+        }else if(pn[m].weight==1){
+            pn[m].weight=2;
+            if(pn[m].rchild){
+                str+="1";
+                m=pn[m].rchild;
+            }
+        }else {
+            m=pn[m].parent;
+            if(str.length()>0) str.pop_back();
         }
-        reverse(str.begin(),str.end());
-        v.push_back(str);
     }
 }
 
 void tranverse(vector<string> &v)
 {
     int len = v.size();
-    for (int i = 0; i < len; ++i)
+    for (int i = 1; i < len; ++i)
     {
         cout << v[i] << endl;
     }
