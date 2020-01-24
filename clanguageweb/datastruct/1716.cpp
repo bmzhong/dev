2cfe3d65b5d9d62f1294ba8feed5e52facc1ac42
menglijinghua <508304482@qq.com>
2020-01-24 15:46:18 +0800

    23

--- clanguageweb/datastruct/1716.cpp
+++ clanguageweb/datastruct/1716.cpp
@@ -1,49 +1,43 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 int partition(int a[], int x, int y)
 {
-    int d = a[x];
+    a[0] = a[x];
     while (x < y)
     {
-        while (x < y && a[y] > d)
+        while (x < y && a[y] > a[0])
             --y;
         a[x] = a[y];
-        while (x < y && a[x] < d)
+        while (x < y && a[x] < a[0])
             ++x;
         a[y] = a[x];
     }
-    a[x] = d;
+    a[x] = a[0];
     return x;
 }
 
 void sort(int a[], int x, int y)
 {
-    int m = partition(a, x, y);
-    if (m - 1 > x)
+    if (y > x)
     {
+        int m = partition(a, x, y);
         sort(a, x, m - 1);
-    }
-    if (m + 1 < y)
-    {
         sort(a, m + 1, y);
     }
 }
 int main()
 {
-    int n, c;
+    int n;
     scanf("%d", &n);
-    int a[n];
-    for (int i = 0; i < n; ++i)
+    int a[n + 1];
+    for (int i = 1; i <= n; ++i)
     {
-        scanf("%d",&c);
-        a[i] = c;
+        scanf("%d", &a[i]);
     }
-    sort(a, 0, n - 1);
-    for (int i = 0; i < n - 1; ++i)
+    sort(a, 1, n);
+    for (int i = 1; i <= n - 1; ++i)
     {
         printf("%d ", a[i]);
     }
-    printf("%d\n", a[n - 1]);
-    system("pause");
+    printf("%d\n", a[n]);
     return 0;
 }
