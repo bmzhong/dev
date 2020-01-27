ca1c50bdfacba04665dec9fd36bca7aa26c48ce4
menglijinghua <508304482@qq.com>
2020-01-27 08:42:46 +0800

    26

--- clanguageweb/other/1118.cpp
+++ clanguageweb/other/1118.cpp
@@ -5,6 +5,13 @@ int main()
 {
     string str;
     while(cin>>str){
-        
+        int len=str.size();
+        int s=0;
+        for(int i=0;i<len;++i){
+            s+=str[i]-'0';
+        }
+        cout<<s<<endl;
     }
+    system("pause");
+    return 0;
 }
