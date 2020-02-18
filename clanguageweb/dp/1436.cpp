9eca87d9fd68b3347896d24dbaaf76e10b3ef0d1
menglijinghua <508304482@qq.com>
2020-02-18 11:25:11 +0800

    18

--- clanguageweb/dp/1436.cpp
+++ clanguageweb/dp/1436.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-int dp[51][51][13], a[51][51], n, m, o, maxs;
+int dp[51][51][13][14], a[51][51], n, m, o, res;
 int main()
 {
     cin >> n >> m >> o;
@@ -10,22 +10,38 @@ int main()
         for (int j = 1; j <= m; ++j)
         {
             cin >> a[i][j];
+            ++a[i][j];
         }
     }
+    dp[1][1][0][0] = 1;
+    dp[1][1][1][a[1][1]] = 1;
     for (int i = 1; i <= n; ++i)
     {
         for (int j = 1; j <= m; ++j)
         {
-            dp[i][j][0] = 1;
-            for (int k = 1; k <= o; ++k)
+            for (int cnt = 0; cnt <= o; ++cnt)
             {
-                dp[i][j][k] = dp[i - 1][j][k] + dp[i][j - 1][k];
-                if (a[i][j] > maxs)
+                for (int k = 0; k <= 14; ++k)
                 {
-                    dp[i][j][k] += dp[i][j - 1][k - 1] + dp[i - 1][j][k - 1];
-                    maxs = max(maxs, a[i][j]);
+                    dp[i][j][cnt][k] = (dp[i][j][cnt][k] + dp[i - 1][j][cnt][k]) % 1000000007;
+                    dp[i][j][cnt][k] = (dp[i][j][cnt][k] + dp[i][j - 1][cnt][k]) % 1000000007;
+                    if (cnt > 0 && k == a[i][j])
+                    {
+                        for (int p = 0; p < a[i][j]; ++p)
+                        {
+                            dp[i][j][cnt][k] = (dp[i][j][cnt][k] + dp[i - 1][j][cnt - 1][p]) % 1000000007;
+                            dp[i][j][cnt][k] = (dp[i][j][cnt][k] + dp[i][j - 1][cnt - 1][p]) % 1000000007;
+                        }
+                    }
                 }
             }
         }
     }
+    for (int i = 1; i <= 14; ++i)
+    {
+        res = (res + dp[n][m][o][i]) % 1000000007;
+    }
+    cout << res << endl;
+    system("pause");
+    return 0;
 }
